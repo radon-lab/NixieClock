87d5d7e5081915d4928af8a0b3dca6a3466fbd44
radon-lab <71728333+radon-lab@users.noreply.github.com>
2022-11-13 12:38:37 +0300

    1.7.4
    Мелкие исправления.

--- NixieClock/SHT.h
+++ NixieClock/SHT.h
@@ -1,19 +1,19 @@
 #define SHT20_ADDR 0x40 //адрес датчика
 
-#define SHT20_WRITE_REG 0xE6
+#define SHT20_WRITE_REG 0xE6 //регистр настроек и статуса
 
-#define SHT20_READ_TEMP 0xF3
-#define SHT20_READ_HUM 0xF5
+#define SHT20_READ_TEMP 0xF3 //начать замер температуры
+#define SHT20_READ_HUM 0xF5 //начать замер влажности
 
-#define SHT20_TEMP_TIME 15
-#define SHT20_HUM_TIME 20
+#define SHT20_TEMP_TIME 15 //таймаут ожидания замера температуры
+#define SHT20_HUM_TIME 20 //таймаут ожидания замера влажности
 
 #define SHT30_ADDR 0x44 //адрес датчика
 
-#define SHT30_MEAS_TIME 20
+#define SHT30_READ_DATA 0x24 //начать замер температуры и влажности
+#define SHT30_RESOLUTION 0x00 //установка точности измерения
 
-#define SHT30_READ_DATA 0x24
-#define SHT30_RESOLUTION 0x00
+#define SHT30_MEAS_TIME 20 //таймаут ожидания замера
 
 //--------------------------------------Чтение температуры/влажности------------------------------------------
 void readTempSHT(void) //чтение температуры/влажности
@@ -48,7 +48,7 @@ void readTempSHT(void) //чтение температуры/влажности
         }
         uint16_t temp_raw = ((uint16_t)wireRead() << 8) | (wireRead() & 0xFC);
         wireReadEndByte(); //пропускаем контрольную сумму
-        sens.temp = temp_raw * 0.0268 - 468; //рассчитываем температуру
+        sens.temp = (uint16_t)(temp_raw * 0.0268) - 468; //рассчитываем температуру
 
         if (wireBeginTransmission(SHT20_ADDR)) return; //начало передачи
         wireWrite(SHT20_READ_HUM); //устанавливаем адрес записи
@@ -61,7 +61,7 @@ void readTempSHT(void) //чтение температуры/влажности
         }
         uint16_t hum_raw = ((uint16_t)wireRead() << 8) | (wireRead() & 0xFC);
         wireReadEndByte(); //пропускаем контрольную сумму
-        sens.hum = hum_raw * 0.0019 - 6; //рассчитываем влажность
+        sens.hum = (uint16_t)(hum_raw * 0.0019) - 6; //рассчитываем влажность
       }
       break;
     case SHT30_ADDR: {
@@ -79,7 +79,7 @@ void readTempSHT(void) //чтение температуры/влажности
         wireRead(); //пропускаем контрольную сумму
         uint16_t hum_raw = ((uint16_t)wireRead() << 8) | wireRead();
         wireReadEndByte(); //пропускаем контрольную сумму
-        sens.temp = temp_raw * 0.0267 - 450;  //рассчитываем температуру
+        sens.temp = (uint16_t)(temp_raw * 0.0267) - 450;  //рассчитываем температуру
         sens.hum = hum_raw * 0.00152; //рассчитываем влажность
       }
       break;
