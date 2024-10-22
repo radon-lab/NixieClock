688a9592d0807b554c5bdfcbbd4d2e893ac791dc
radon-lab <71728333+radon-lab@users.noreply.github.com>
2024-10-22 08:05:51 +0300

    2.2.4
    Мелкие исправления.

--- NixieClock_Web_UI/NixieClock_Web_UI.ino
+++ NixieClock_Web_UI/NixieClock_Web_UI.ino
@@ -1,5 +1,5 @@
 /*
-  Arduino IDE 1.8.13 версия прошивки 1.2.4 релиз от 21.10.24
+  Arduino IDE 1.8.13 версия прошивки 1.2.4 релиз от 22.10.24
   Специльно для проекта "Часы на ГРИ v2. Альтернативная прошивка"
   Страница проекта - https://community.alexgyver.ru/threads/chasy-na-gri-v2-alternativnaja-proshivka.5843/
 
